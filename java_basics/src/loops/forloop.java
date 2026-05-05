0f298c30d1337a799e0dbc6cb1106db1d514d6b1
Gourav Sharma <sharmagourav687526@gmail.com>
2026-05-05 13:13:52 +0530

    doing question today of patterns

--- java_basics/src/loops/forloop.java
+++ java_basics/src/loops/forloop.java
@@ -3,7 +3,7 @@ package loops;
 public class forloop {
     public static void main(String[] args) {
         for(int i=1;i<=5;i++){
-            
+            System.out.println(" hello world"+i);
         }
     }
 }
