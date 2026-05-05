594e95100e57e9cc771bf9c22b9f10e95e84dba3
Gourav Sharma <sharmagourav687526@gmail.com>
2026-05-05 13:12:44 +0530

    doing question today of patterns

---
+++ java_basics/src/patterens/solidRectangle.java
@@ -0,0 +1,14 @@
+package patterens;
+
+public class solidRectangle {
+    public static void main(String[] args) {
+        int rows = 4;
+        int cols = 5;
+         for(int i=1;i<=rows;i++){
+             for(int j=1;j<=cols;j++){
+                 System.out.print("* ");
+             }
+             System.out.println();
+         }
+    }
+}
